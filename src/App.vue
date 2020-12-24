b2a83b0d974d04456de4a248bd5e9fbc188f44a6
maggiemullin <maggiemullin1@gmail.com>
2020-12-24 18:38:25 -0500

    new

--- src/App.vue
+++ src/App.vue
@@ -96,7 +96,7 @@ export default {
   methods: {
     fetchCities() {
       this.cities =
-      ["Barrie", "Antarctica", "Montego Bay", "London". "Las Vegas", "Feversham"];
+      ["Barrie", "Antarctica", "Montego Bay", "London", "Las Vegas", "Feversham"];
     },
 
     getRandomInt(max = 128){
